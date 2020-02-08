2f324c1b64e165965b15213ad2648c9a4ad32acd
voins <voins@qq.com>
2020-02-08 22:14:41 +0800

    注解版mybatis完结 （spring-boot-06-data-mybatis）

--- spring-boot-06-data-mybatis/src/main/java/com/explore/springboot/config/MybatisConfig.java
+++ spring-boot-06-data-mybatis/src/main/java/com/explore/springboot/config/MybatisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 public class MybatisConfig {
     /**
      * 设置表的下划线与bean的大写字母对应
+     * 此设置对写在xml的sql不起作用，需在mybatis-config.xml中配置此属性标签<setting name="mapUnderscoreToCamelCase" value="true"></setting>
      */
     @Bean
     public ConfigurationCustomizer configurationCustomizer(){
